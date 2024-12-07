88fa73f0091b84d9128ed472e446df280da10e0b
Arpan-Shah-102 <arpanshah102@gmail.com>
2024-12-07 13:42:08 -0500

    almost done

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
-spin = document.querySelector('.spin');
-autoSpin = document.querySelector('.auto-spin');
+let totalSpins = 0;
+let money = 100000;
 
-terminal = document.querySelector('.terminal');
-moneySpent = document.querySelector('.money > span');
-money = 1000
+let jackpotPrize = 300;
+let twoPairPrize = 10;
+let addedBonus = 0;
 
-a = document.querySelector('.a');
-b = document.querySelector('.b');
-c = document.querySelector('.c');
+let upgradeLuck = document.querySelector(".luck");
+let upgradeIncome = document.querySelector(".income");
+let upgradeSpin = document.querySelector(".spin-upg");
 
-body = document.querySelector('body');
-colorThemes = document.querySelector('.color-themes');
+let upgradeLuckLevel = 1;
+let upgradeIncomeLevel = 1;
+let upgradeSpinLevel = 1;
 
-slotSound = new Audio('./assets/slot-spin-sound.mp3');
-slotPayout = new Audio('./assets/slot-payout.mp3');
-jackpot = new Audio('./assets/jackpot.mp3');
+let upgradeLuckPrice = 500;
+let upgradeIncomePrice = 500;
+let upgradeSpinPrice = 500;
 
-spin.addEventListener("click", function() {spinSlot(1, false)});
+let upgradeLuckContainer = document.querySelector(".luck-cont");
+let upgradeIncomeContainer = document.querySelector(".income-cont");
+let upgradeSpinContainer = document.querySelector(".spin-cont");
+
+let spin = document.querySelector('.spin');
+let autoSpin = document.querySelector('.auto-spin');
+
+let terminal = document.querySelector('.terminal');
+let moneySpent = document.querySelector('.money > span');
+
+let a = document.querySelector('.a');
+let b = document.querySelector('.b');
+let c = document.querySelector('.c');
+
+let body = document.querySelector('body');
+let colorThemes = document.querySelector('.color-themes');
+
+const slotSound = new Audio('./assets/slot-spin-sound.mp3');
+const slotPayout = new Audio('./assets/slot-payout.mp3');
+const jackpot = new Audio('./assets/jackpot.mp3');
+
+let prizeLog = document.querySelector('.prize-container');
+
+spin.addEventListener("click", function() {
+  try {
+    if (10 > money) {
+      throw new Error("You do not have enough money. You are broke.");
+    }
+    spinSlot(1, false);
+  }
+  catch (err) {
+    alert("You do not have enough money. You are broke.");
+    console.log(err);
+  }
+});
 autoSpin.addEventListener("click", function() {
   try {
     numberOfItereations = Number(prompt("How many times would you like to spin?"));
     if (isNaN(numberOfItereations) || numberOfItereations <= 0) {
       throw new Error("Invalid number of iterations. Please enter a positive number.");
+    } else if (numberOfItereations * 10 > money) {
+      throw new Error("You don't have enough money to perform that many spins.");
     }
     spinSlot(numberOfItereations, true);
   }
@@ -31,7 +68,7 @@ autoSpin.addEventListener("click", function() {
   }
 });
 
-symbols = ['🍒','💎','🔔','🍉','⭐','🍇','7️⃣','🥇','🥈','🥉', '🍋', '🪙', '🥭'];
+symbols = ['7️⃣','🍋','🪙','🥭','🔔','🍉','⭐','🍇','🥇','🥈','🥉','🍒','💎'];
 function spinSlot(iterations, autoSpinOn) {
   let completedSpins = 0;
   let multiplier;
@@ -48,7 +85,8 @@ function spinSlot(iterations, autoSpinOn) {
       autoSpin.disabled = true;
 
       money -= 10;
-      moneySpent.innerText = money;
+      money += addedBonus;
+      moneySpent.innerText = Math.floor(money);
 
       let s1;
       let s2;
@@ -66,17 +104,27 @@ function spinSlot(iterations, autoSpinOn) {
         }, 100 * i * multiplier);
       }
       setTimeout(function() {
+        let moneyEarned = 0;
+        totalSpins++;
+        let tag = document.createElement('p');
+
         if (s1 == s2 && s2 == s3) {
-          money += 300
+          money += jackpotPrize;
+          moneyEarned += jackpotPrize;
           terminal.innerText = "Congratulations! You won $300!";
-          moneySpent.innerText = money;
+          moneySpent.innerText = Math.floor(money);
           jackpot.play();
+          tag.innerText = `Spin ${totalSpins} - $${moneyEarned} - ${s1} ${s2} ${s3}`;
+          prizeLog.insertBefore(tag, prizeLog.firstChild);
         }
         else if (s1 == s2 || s1 == s3 || s2 == s3) {
-          money += 10
+          money += twoPairPrize;
+          moneyEarned += twoPairPrize;
           terminal.innerText = "Congratulations! You won $10!";
-          moneySpent.innerText = money;
+          moneySpent.innerText = Math.floor(money);
           slotPayout.play();
+          tag.innerText = `Spin ${totalSpins} - $${moneyEarned} - ${s1} ${s2} ${s3}`;
+          prizeLog.insertBefore(tag, prizeLog.firstChild);
         }
         else {
           terminal.innerText = "Sorry, you didn't win.";
@@ -92,4 +140,66 @@ function spinSlot(iterations, autoSpinOn) {
   }
 }
 
-colorThemes.addEventListener("click", function () {body.classList.toggle("dark");});
+colorThemes.addEventListener("click", function () {
+  body.classList.toggle("dark");
+});
+
+upgradeLuck.addEventListener("click", function() {
+  if (money >= upgradeLuckPrice) {
+    if (upgradeLuckLevel < 9) {
+      money -= upgradeLuckPrice;
+      moneySpent.innerText = Math.floor(money);
+
+      upgradeLuckLevel += 1;
+      upgradeLuckPrice += 500;
+      upgradeLuckContainer.querySelector('p').innerText = `Lv. ${upgradeLuckLevel} > Lv. ${upgradeLuckLevel + 1}`;
+      upgradeLuck.innerText = `$${upgradeLuckPrice}`;
+    } else {
+      upgradeLuckContainer.querySelector('p').innerText = `Max Level (Lv. 10)`;
+      upgradeLuck.disabled = true;
+    }
+  } else {
+    alert("You don't have enough money to upgrade this feature.");
+  }
+});
+upgradeIncome.addEventListener("click", function() {
+  if (money >= upgradeIncomePrice) {
+    if (upgradeIncomeLevel < 9) {
+      money -= upgradeIncomePrice;
+      moneySpent.innerText = Math.floor(money);
+      jackpotPrize *= 1.5;
+      twoPairPrize *= 1.5;
+
+      upgradeIncomeLevel += 1;
+      upgradeIncomePrice += 500;
+      upgradeIncomeContainer.querySelector('p').innerText = `Lv. ${upgradeIncomeLevel} > Lv. ${upgradeIncomeLevel + 1}`;
+      upgradeIncome.innerText = `$${upgradeIncomePrice}`;
+    } else {
+      upgradeIncomeContainer.querySelector('p').innerText = `Max Level (Lv. 10)`;
+      upgradeIncome.disabled = true;
+    }
+  } else {
+    alert("You don't have enough money to upgrade this feature.");
+  }
+});
+upgradeSpin.addEventListener("click", function() {
+  if (money >= upgradeSpinPrice) {
+    if (upgradeSpinLevel < 9) {
+      money -= upgradeSpinPrice;
+      moneySpent.innerText = Math.floor(money);
+      addedBonus = addedBonus + 1.25;
+
+      console.log(addedBonus);
+
+      upgradeSpinLevel += 1;
+      upgradeSpinPrice += 500;
+      upgradeSpinContainer.querySelector('p').innerText = `Lv. ${upgradeSpinLevel} > Lv. ${upgradeSpinLevel + 1}`;
+      upgradeSpin.innerText = `$${upgradeSpinPrice}`;
+    } else {
+      upgradeSpinContainer.querySelector('p').innerText = `Max Level (Lv. 10)`;
+      upgradeSpin.disabled = true;
+    }
+  } else {
+    alert("You don't have enough money to upgrade this feature.");
+  }
+});
